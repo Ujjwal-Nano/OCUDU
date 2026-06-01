3bd09d60e1a50c538a5a0df92f44441f08e9db2e
Jonathan Pichel Carrera <jonathan.pichel@srs.io>
2026-06-01 18:24:52 +0200

    sched: move pucch ue resource ownership tracking into collision_manager
    Signed-off-by: Jonathan Pichel Carrera <jonathan.pichel@srs.io>

--- lib/scheduler/pucch_scheduling/pucch_allocator_impl.cpp
+++ lib/scheduler/pucch_scheduling/pucch_allocator_impl.cpp
@@ -14,6 +14,7 @@
 #include "ocudu/ran/pucch/pucch_uci_bits.h"
 #include "ocudu/ran/resource_allocation/ofdm_symbol_range.h"
 #include "ocudu/scheduler/config/serving_cell_config_factory.h"
+#include "ocudu/scheduler/resource_grid_util.h"
 #include "ocudu/scheduler/result/sched_result.h"
 #include "ocudu/support/ocudu_assert.h"
 #include "fmt/std.h"
@@ -210,7 +211,8 @@ std::optional<unsigned> pucch_allocator_impl::alloc_common_harq_ack(cell_resourc
   }
 
   // Try to get an available PUCCH common resource for HARQ-ACK.
-  std::optional<pucch_common_params> pucch_res = alloc_pucch_common_res_harq(pucch_slot_alloc, dci_info.ctx);
+  std::optional<pucch_common_params> pucch_res =
+      alloc_pucch_common_res_harq(pucch_slot_alloc, dci_info.ctx, alloc_ctx.rnti);
 
   if (not pucch_res.has_value()) {
     alloc_ctx.log_skipped_alloc(logger.debug, "no resources available");
@@ -573,7 +575,8 @@ unsigned pucch_allocator_impl::pucch_grant_list::get_nof_grants() const
 // if no resource is available.
 std::optional<pucch_allocator_impl::pucch_common_params>
 pucch_allocator_impl::alloc_pucch_common_res_harq(cell_slot_resource_allocator&  pucch_slot_alloc,
-                                                  const dci_context_information& dci_info)
+                                                  const dci_context_information& dci_info,
+                                                  rnti_t                         rnti)
 {
   // Get N_CCE (nof_coreset_cces) and n_{CCE,0} (start_cce_idx), as per TS 38.213, Section 9.2.1.
   const unsigned nof_coreset_cces = dci_info.coreset_cfg->get_nof_cces();
@@ -588,7 +591,7 @@ pucch_allocator_impl::alloc_pucch_common_res_harq(cell_slot_resource_allocator& 
     ocudu_assert(r_pucch < 16, "r_PUCCH must be less than 16");
 
     // Look for an available PUCCH common resource.
-    if (not resource_manager.reserve_harq_common_resource(pucch_slot_alloc, r_pucch)) {
+    if (not resource_manager.reserve_harq_common_resource(pucch_slot_alloc, r_pucch, rnti)) {
       continue;
     }
 
@@ -646,14 +649,14 @@ pucch_allocator_impl::find_common_and_ded_harq_res_available(cell_slot_resource_
 
     // Look for an available PUCCH common resource.
     // TODO: allow collisions between common and dedicated resources for the same UE, since it will never send both.
-    if (not resource_manager.reserve_harq_common_resource(pucch_slot_alloc, r_pucch)) {
+    if (not resource_manager.reserve_harq_common_resource(pucch_slot_alloc, r_pucch, alloc_ctx.rnti)) {
       continue;
     }
 
     // Look for an available PUCCH dedicated resource with the same PUCCH resource indicator as the common's.
     const pucch_resource* ded_res = guard.reserve_harq_set_0_resource_by_res_indicator(d_pri);
     if (ded_res == nullptr) {
-      resource_manager.release_harq_common_resource(pucch_slot_alloc, r_pucch);
+      resource_manager.release_harq_common_resource(pucch_slot_alloc, r_pucch, alloc_ctx.rnti);
       continue;
     }
 
@@ -674,7 +677,7 @@ pucch_allocator_impl::find_common_and_ded_harq_res_available(cell_slot_resource_
     // pre-allocated are constrained in the PUCCH resource indicator. Therefore, the \ref multiplex_and_allocate_pucch
     // function might not preserve the requested PUCCH resource indicator \ref d_pri.
     if (not d_pri_ded.has_value() or (cell_cfg.is_pucch_f0_and_f2() and *d_pri_ded != d_pri)) {
-      resource_manager.release_harq_common_resource(pucch_slot_alloc, r_pucch);
+      resource_manager.release_harq_common_resource(pucch_slot_alloc, r_pucch, alloc_ctx.rnti);
       continue;
     }
     ocudu_assert(*d_pri_ded == d_pri, "The PUCCH resource indicator must match for common and ded. resources");
@@ -1653,7 +1656,7 @@ void pucch_allocator_impl::fill_ded_pdu(pucch_info&           pucch_pdu,
       const auto max_c_rate = to_float(cell_cfg.params.init_bwp.pucch.resources.max_code_rate_234());
       f2.nof_prbs           = adjust_prbs
                                   ? get_pucch_format2_nof_prbs(
-                              uci_bits.get_total_bits(), pucch_res.prbs().length(), pucch_res.syms.length(), max_c_rate)
+                                        uci_bits.get_total_bits(), pucch_res.prbs().length(), pucch_res.syms.length(), max_c_rate)
                                   : pucch_res.prbs().length();
     } break;
     case pucch_format::FORMAT_3: {
@@ -1670,12 +1673,12 @@ void pucch_allocator_impl::fill_ded_pdu(pucch_info&           pucch_pdu,
       f3.n_id_0_scrambling  = n_id_0_scrambling();
       const auto max_c_rate = to_float(cell_cfg.params.init_bwp.pucch.resources.max_code_rate_234());
       f3.nof_prbs           = adjust_prbs ? get_pucch_format3_nof_prbs(uci_bits.get_total_bits(),
-                                                             pucch_res.prbs().length(),
-                                                             pucch_res.syms.length(),
-                                                             max_c_rate,
-                                                             pucch_res.second_hop_prb.has_value(),
-                                                             res_f3.additional_dmrs,
-                                                             res_f3.pi_2_bpsk)
+                                                                       pucch_res.prbs().length(),
+                                                                       pucch_res.syms.length(),
+                                                                       max_c_rate,
+                                                                       pucch_res.second_hop_prb.has_value(),
+                                                                       res_f3.additional_dmrs,
+                                                                       res_f3.pi_2_bpsk)
                                           : pucch_res.prbs().length();
     } break;
     case pucch_format::FORMAT_4: {
